e52a52e5fee6e926547c3b000fcd524c32016102
Nikita Degtiarev <ndegtiarev@plesk.com>
2023-03-01 15:35:01 +0200

    fix for plesk

--- symfony/framework-bundle/5.4/src/Kernel.php
+++ symfony/framework-bundle/5.4/src/Kernel.php
@@ -1,6 +1,7 @@
 <?php
+// Copyright 1999-2023. Plesk International GmbH. All rights reserved.
 
-namespace App;
+namespace PleskExt\Demo;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
@@ -8,4 +9,9 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
